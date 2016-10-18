dcfa2c1810d227fa6a3bb003f7b4be6c38c8f8eb
James <jame2408@gmail.com>
2016-10-18 11:35:20 +0800

    第一集買一本, 總價100元(補簽入程式部份)

---
+++ TDD_Katas_BuyHarryPotterBooks/CalculatorPrice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Katas_BuyHarryPotterBooks
+{
+    public class CalculatorPrice
+    {
+        const double OneBookPrice = 100;
+
+        public double BuyBooksPrice(List<int> BooksNumbers)
+        {
+            double price = 0;
+            for (int i = 0; i < BooksNumbers.Count; i++)
+            {
+                if (BooksNumbers[i] > 0)
+                {
+                    price += BooksNumbers[i] * OneBookPrice;
+                }
+            }
+            return price;
+        }
+    }
+}
