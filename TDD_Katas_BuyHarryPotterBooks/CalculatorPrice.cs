33f6cf51abfd153e09febf3a7ce2e71d3476b1cd
James <jame2408@gmail.com>
2016-10-18 11:55:06 +0800

    依據『第1集與第2集各買一本_總價190元_測試5啪折扣』測試個案，調整程式碼（綠燈）

--- TDD_Katas_BuyHarryPotterBooks/CalculatorPrice.cs
+++ TDD_Katas_BuyHarryPotterBooks/CalculatorPrice.cs
@@ -9,16 +9,24 @@ namespace TDD_Katas_BuyHarryPotterBooks
     public class CalculatorPrice
     {
         const double OneBookPrice = 100;
+        const double DISCOUNTS_5 = 0.05;
 
         public double BuyBooksPrice(List<int> BooksNumbers)
         {
             double price = 0;
+            int DifferentEpisode = 0;
+
             for (int i = 0; i < BooksNumbers.Count; i++)
             {
                 if (BooksNumbers[i] > 0)
                 {
                     price += BooksNumbers[i] * OneBookPrice;
-                }
+                    DifferentEpisode++;
+                }                
+            }
+            if (DifferentEpisode == 2)
+            {
+                price = price * (1 - DISCOUNTS_5);
             }
             return price;
         }
