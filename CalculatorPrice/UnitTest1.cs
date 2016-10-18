df6a8c6e7a3e502878bdf68875cd584a2b68c239
James <jame2408@gmail.com>
2016-10-18 11:34:27 +0800

    第一集買一本, 總價100元

--- CalculatorPrice/UnitTest1.cs
+++ CalculatorPrice/UnitTest1.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Katas_BuyHarryPotterBooks;
+using System.Collections.Generic;
 
-namespace CalculatorPrice
+namespace TDD_Katas_BuyHarryPotterBooks.Tests
 {
-    [TestClass]
-    public class UnitTest1
+    /// <summary>
+    /// 有一家書店在賣哈利波特書籍系列，
+    /// 每一本書定價 $100 元。
+    /// 買兩本不同的書可以打5% 的折扣、
+    /// 買三本不同的書可以打 10% 的折扣、
+    /// 買四本不同的書可以打 20%、
+    /// 如果買到五本可以打到 25% 的折扣
+    /// </summary>
+    [TestClass()]
+    public class BuyBooksPriceTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod()]
+        public void BuyBooksPriceTest_第一集買一本_總價100元()
         {
+            //1. Arrange
+            var BooksNumbers = new List<int> { 1, 0, 0, 0, 0};            
+
+            var target = new TDD_Katas_BuyHarryPotterBooks.CalculatorPrice();
+
+            double expected = 100;
+            
+            //2. Act
+            double actual;
+            actual = target.BuyBooksPrice(BooksNumbers);
+
+            //3. Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
+
