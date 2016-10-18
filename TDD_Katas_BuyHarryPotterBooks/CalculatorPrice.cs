fd587c494a5d102197f02dc9f8bf6cfb41837bf6
James <jame2408@gmail.com>
2016-10-18 12:01:56 +0800

    重構, 將計算總價格拉出來成為Method (綠燈)

--- TDD_Katas_BuyHarryPotterBooks/CalculatorPrice.cs
+++ TDD_Katas_BuyHarryPotterBooks/CalculatorPrice.cs
@@ -16,19 +16,25 @@ namespace TDD_Katas_BuyHarryPotterBooks
             double price = 0;
             int DifferentEpisode = 0;
 
+            CalculatorTotalPrice(BooksNumbers, ref price, ref DifferentEpisode);
+
+            return price;
+        }
+
+        private static void CalculatorTotalPrice(List<int> BooksNumbers, ref double price, ref int DifferentEpisode)
+        {
             for (int i = 0; i < BooksNumbers.Count; i++)
             {
                 if (BooksNumbers[i] > 0)
                 {
                     price += BooksNumbers[i] * OneBookPrice;
                     DifferentEpisode++;
-                }                
+                }
             }
             if (DifferentEpisode == 2)
             {
                 price = price * (1 - DISCOUNTS_5);
             }
-            return price;
         }
     }
 }
