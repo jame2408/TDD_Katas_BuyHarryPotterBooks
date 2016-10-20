c5a4d53d2b32cee4a56e49cff3446bbe05fb73e9
James <jame2408@gmail.com>
2016-10-20 15:20:47 +0800

    新增『第1集到第4集各買一本_總價320元_測試20啪折扣』測試程式（紅燈）

--- CalculatorPrice/UnitTest1.cs
+++ CalculatorPrice/UnitTest1.cs
@@ -69,6 +69,24 @@ namespace TDD_Katas_BuyHarryPotterBooks.Tests
             //3. Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void BuyBooksPriceTest_第1集到第4集各買一本_總價320元_測試20啪折扣()
+        {
+            //1. Arrange
+            var BooksNumbers = new List<int> { 1, 1, 1, 1, 0 };
+
+            var target = new TDD_Katas_BuyHarryPotterBooks.CalculatorPrice();
+
+            double expected = 320;
+
+            //2. Act
+            double actual;
+            actual = target.BuyBooksPrice(BooksNumbers);
+
+            //3. Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
 
